85f6ff8d2d5248b9617438697b64a4140bdf52d3
M.Khushnood Saeed <khushnoodsaeed21@gmail.com>
2023-11-24 14:26:40 +0500

    Form

--- Client/App.js
+++ Client/App.js
@@ -1,23 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, StyleSheet, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context'
+import Signin from './src/screens/Signin';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-
-      <Text>Bismillah!</Text>
-      <Text>Welcome to intrax</Text>
-
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <Signin/>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+      flex:1,
+      marginTop:StatusBar.currentHeight
+  }})
